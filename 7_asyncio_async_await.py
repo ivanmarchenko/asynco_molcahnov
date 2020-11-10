270be4203e7ea37c2ec2f368e30c7e1d481e1f4c
ivanmarchenko <marchenkoivanu@gmail.com>
2020-11-10 13:03:01 +0300

    7 asyncio

---
+++ 7_asyncio_async_await.py
@@ -0,0 +1,41 @@
+import asyncio
+from time import time
+
+
+# @asyncio.coroutine
+async def print_nums():
+    num = 1
+    while True:
+        print(num)
+        num += 1
+        await asyncio.sleep(0.5)
+        # yield from asyncio.sleep(0.5)
+
+
+# @asyncio.coroutine
+async def print_time():
+    count = 0
+    while True:
+        if count % 3 == 0:
+            print("{} seconds have passed.".format(count))
+        count += 1
+        await asyncio.sleep(1)
+        # yield from asyncio.sleep(1)
+        
+
+# @asyncio.coroutine
+async def main():
+    # task1 = asyncio.ensure_future(print_nums())
+    task1= asyncio.create_task(print_nums())
+    # task2 = asyncio.ensure_future(print_time())
+    task2 = asyncio.create_task(print_time())
+
+    await asyncio.gather(task1, task2)
+    # yield from asyncio.gather(task1, task2)
+
+
+if __name__ == '__main__':
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main())
+    # loop.close()
+    asyncio.run(main())
